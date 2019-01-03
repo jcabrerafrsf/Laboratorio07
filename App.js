b98530b1dd3d4ac5a539c58d912c964feffcdbcd
Fausto <faustofedele2013@gmail.com>
2019-01-03 18:05:48 -0300

    Subo lo mio

--- App.js
+++ App.js
@@ -9,7 +9,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import AppBanco from './src/AppBanco.js';
-
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
--- src/AppBanco.js
+++ src/AppBanco.js
@@ -2,77 +2,119 @@ import React, {Component} from 'react';
 import {ToastAndroid,Button, StyleSheet, Text, TextInput, Picker, View,
 Switch, CheckBox, Slider} from 'react-native';
 export default class AppBanco extends Component {
- constructor(props) {
- super(props);
- this.state = {
- moneda:1,
- capitalInicial:0,
- capitalFinal:0
- };
- this.hacerPlazoFijo = this.hacerPlazoFijo.bind(this);
- }
- hacerPlazoFijo(){
- ToastAndroid.show('Presiono el boton de hacer plazo fijo!',
-ToastAndroid.LONG);
- }
+	constructor(props) {
+		super(props);
+		this.state = {
+			moneda:1,
+			capitalInicial:0,
+			capitalFinal:0,
+			dias:1,
+			monto:0
+		};
+		this.hacerPlazoFijo = this.hacerPlazoFijo.bind(this);
+	}
+	hacerPlazoFijo(){
+		ToastAndroid.show('Presiono el boton de hacer plazo fijo!',
+		ToastAndroid.LONG);
+	}
 
- render() {
- return (
- <View style={styles.container}>
- <Text>Correo Electronico</Text>
- <TextInput>correo@mail.com</TextInput>
- <Text>CUIT</Text>
- <TextInput>00-00000000-0</TextInput>
- <Text>Moneda</Text>
- <Picker
- style={{width: 200}}
- selectedValue={this.state.moneda}
- onValueChange={(valor) => this.setState({moneda:valor})}>
- <Picker.Item label="Dolar" value="1" />
- <Picker.Item label="Pesos ARS" value="2" />
- </Picker>
- <Text>Monto</Text>
- <TextInput>000</TextInput>
- <Text>Dias</Text>
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>
+					Correo Electronico
+				</Text>
+				<TextInput
+				keyboardType='email-address'>
+					correo@gmail.com
+				</TextInput>
+				<Text>
+					CUIT
+				</Text>
+				<TextInput 
+				keyboardType='numeric'>
+					00-00000000-0
+				</TextInput>
+				<Text>
+					Moneda
+				</Text>
+				<Picker
+				style={{width: 200}}
+				selectedValue={this.state.moneda}
+				onValueChange={(valor) => this.setState({moneda:valor})}>
+					<Picker.Item label="Dolar" value="1" />
+					<Picker.Item label="Pesos ARS" value="2" />
+				</Picker>
+				<Text>
+					Monto
+				</Text>
+				<TextInput
+				keyboardType='numeric'
+				onChangeText={(text) => this.setState({ monto: text})}
+				value = {this.state.monto}>
+					0
+				</TextInput>
+				<Text>
+					Dias
+				</Text>
+				<Slider 
+				minimumValue={1}
+				maximumValue={30}
+				style={{ width: 300 }}
+				step={1}
+				value={this.state.dias}
+				onSlidingComplete={val => this.setState({ dias: val })}>
 
-<Slider 
-	minimumValue={1}
-	maximumValue={30}
-	style={{ width: 300 }}
-	step={1}
-	value={this.state.dias}
-	onValueChange={val => this.setState({ dias: val })}>
-</Slider>
+				</Slider>
+				<Text>
+					{this.state.dias} dias
+				</Text>
+				<Text>
+					Avisar por mail
+				</Text>
+				<Switch>
 
- <Text>10 dias</Text>
- <Switch></Switch>
- <Text>Avisar por mail</Text>
- <CheckBox title='Acepto condiciones'/>
- <Button title="Hacer Plazo Fijo"
- color="#FF0000"
- onPress={this.hacerPlazoFijo}>
- </Button>
- <Text>[[ RESULTADO DE LA OPERACION ]]</Text>
- </View>
- );
- }
+				</Switch>
+				<View style={{ flexDirection: 'row' }}>
+				    <CheckBox
+				      value={this.state.checked}
+				      onValueChange={() => this.setState({ checked: !this.state.checked })}
+				    />
+				    <Text style={{marginTop: 5}}> 
+				    	Acepto términos y condiciones
+				    </Text>
+			  	</View>
+				<Button title="Hacer Plazo Fijo"
+				color="#FF0000"
+				onPress={this.hacerPlazoFijo}>
+					
+				</Button>
+				<Text>
+					El plazo fijo con un monto de ${this.state.monto} por el plazo de {this.state.dias} días se realizó exitosamente.
+				</Text>
+				<Text>
+					Obtendrá una ganancia de ${this.state.capitalFinal}.
+				</Text>
+			</View>
+		);
+	}
 }
 const styles = StyleSheet.create({
- container: {
- flex: 1,
- flexDirection: 'column',
- justifyContent: 'flex-start',
- alignItems: 'flex-start',
- backgroundColor: '#F5FCFF',
- },
- welcome: {
- fontSize: 20,
- textAlign: 'center',
- margin: 10,
- },
- instructions: {
- textAlign: 'center',
- color: '#333333',
- marginBottom: 5,
- },
- });
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'flex-start',
+		alignItems: 'flex-start',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
+});
